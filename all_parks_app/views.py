84fb09b5221bdd4b5c2aff115fab51a73d859249
ASand-design <59930866+ASand-design@users.noreply.github.com>
2020-06-29 12:49:31 -0700

    initial commit

--- all_parks_app/views.py
+++ all_parks_app/views.py
@@ -3,6 +3,34 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request, 'index.html')
 
+def login(request):
+    
+    user, errors = User.objects.login_validate(request.POST)
+
+    if errors:
+        for user,error in errors.items():
+            messages.error(request, error)
+        return redirect("/login")
+    else:
+        request.session['user_id'] = user.id
+        return redirect("/welcome")
+
+def register(request):
+    
+    errors = User.objects.register_validate(request.POST)
+
+    if errors:
+        for user,error in errors.items():
+            messages.error(request, error)
+        return redirect("/login")
+    else:
+        user = User.objects.create(first_name=request.POST['first_name'], 
+                                    last_name=request.POST['last_name'],
+                                    email=request.POST['email'],
+                                    password=bcrypt.hashpw(request.POST['password'].encode(),bcrypt.gensalt()).decode())
+        request.session['user_id'] = user.id
+        return redirect("/welcome")
+
 def about(request):
     return render(request, 'about.html')
 
